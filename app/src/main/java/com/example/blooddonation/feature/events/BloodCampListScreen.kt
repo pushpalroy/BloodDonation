ad46553463174063b7de4ee68be938999c09ba5b
Pushpal Roy <pushpalroy2007@gmail.com>
2025-06-08 13:47:57 +0530

    Fix conflicts

--- app/src/main/java/com/example/blooddonation/feature/events/BloodCampListScreen.kt
+++ app/src/main/java/com/example/blooddonation/feature/events/BloodCampListScreen.kt
@@ -1,7 +1,6 @@
 package com.example.blooddonation.feature.events
 
 import BloodCampViewModel
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,23 +8,23 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,12 +37,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.blooddonation.domain.BloodCamp
-import com.example.blooddonation.feature.events.CampCard
 import com.example.blooddonation.feature.theme.ThemeSwitch
 
-@Composable
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun BloodCampListScreen(onBack: () -> Unit = {}, viewModel: BloodCampViewModel = viewModel()) {
     val camps by viewModel.camps.collectAsStateWithLifecycle()
     val registeredCampIds by viewModel.registeredCampIds.collectAsStateWithLifecycle()
